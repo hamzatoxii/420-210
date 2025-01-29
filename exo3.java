2ceb09fc8c7c0945f831cc24ea9f6420b16fd146
Hamza Abou-El Houda <6232753@collegemv.qc.ca>
2025-01-29 10:48:01 -0500

    add ouvrirTerminal

---
+++ exo3.java
@@ -0,0 +1,26 @@
+//--------------------------------------------------------------------
+// Créer et afficher les variables avec le meilleur type possible
+//--------------------------------------------------------------------
+        
+// Variable contenant l'âge d'une personne
+
+// Population du cégep Marie-Victorin
+
+// Variable comptant la population au Qc
+
+// Variable pour calculer le revenu d'un individu
+
+// Nombre de gouttes d'eau dans une piscine.
+
+// Une variable qui se rappelle si le prof était bon ou pas
+
+// Note en lettre qu'un étudiant obtient au cours
+    
+// Les mois de l'année sous format texte
+
+public class HelloWorld{
+	public static void main(String[] args){
+		age = 20;
+		System.out.println(age);
+	}
+}
